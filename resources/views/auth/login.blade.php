456802e6aa95d35b9811d36c052ec6ab23a0fc65
TanNicholas80 <tannicholas54@gmail.com>
2026-01-24 08:34:00 +0700

    Login Tampilan Baru

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -196,7 +196,7 @@
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <title>Login - Taka Dashboard</title>
+    <title>Login - Taka Dashboards</title>
     <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
